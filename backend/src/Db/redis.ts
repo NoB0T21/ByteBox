7173d19bd2aa49a3e2b83124466fb4a11aeac45c
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2025-12-08 11:40:44 +0530

    up

--- backend/src/Db/redis.ts
+++ backend/src/Db/redis.ts
@@ -1,13 +1,10 @@
 import Redis from 'ioredis';
+import dotenv from 'dotenv'
+dotenv.config()
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST||"127.0.0.1",
-  port: Number(process.env.REDIS_PORT || 6379),
-  retryStrategy(times) {
-    return Math.min(times * 50, 2000); // exponential backoff
-  },
-});
+const redis = new Redis(process.env.REDIS_URL||'');
 
+redis.set('foo', 'bar');
 redis.on("connect", () => console.log("✅ Redis connected"));
 redis.on("error", (err) => console.error("❌ Redis error:", err));
 
