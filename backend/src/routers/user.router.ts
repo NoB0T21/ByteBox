09bd438b7109cd47d3503d2b6ce80ecedd062637
Aryan Gawade <147228189+NoB0T21@users.noreply.github.com>
2026-01-16 12:09:30 +0530

    update ratelimiting

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -6,10 +6,10 @@ import {RateLimiter} from "../middleware/rate.limiter";
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() })
 
-router.post('/signup',RateLimiter(10,2),upload.single('file'),register)
-router.post('/web/signup',RateLimiter(10,2),upload.single('file'),register)
-router.post('/signin',RateLimiter(10,2),login)
-router.get('/valid',RateLimiter(60,15),valid)
+router.post('/signup',RateLimiter(10,9),upload.single('file'),register)
+router.post('/web/signup',RateLimiter(10,9),upload.single('file'),register)
+router.post('/signin',RateLimiter(10,9),login)
+router.get('/valid',RateLimiter(60,20),valid)
 router.get('/getuser/:id',middleware,getuserbyid)
 router.get('/getuser',middleware,getUser)
 router.post('/getshareUSer',middleware,getuser)
